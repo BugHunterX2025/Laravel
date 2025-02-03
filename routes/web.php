811397b4b2569d6d64215ca29a8f2d3d2138541d
BugHunterX2025 <devsaurabh@sheelinfotech.in>
2025-02-03 16:51:29 +0530

    add some routes topic

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/post', function () {
+//     // return view('post');
+//     return "<h1>Direct define</h1>";
+//     // return response()->make('<h1>This is my route page</h1>');
+// });
+
+Route::view('post', '/post');
